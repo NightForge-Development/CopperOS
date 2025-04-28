e7c0242ba619133f8d30194c22d62208f7b838be
Scythe | Nyxon <159197373+ScytheMediaTheDev@users.noreply.github.com>
2025-04-28 18:35:25 -0400

    Update boot64.asm

--- boot64.asm
+++ boot64.asm
@@ -3,12 +3,10 @@
 [bits 64]
 [org 0x9000]
 
-; Constants
 KERNEL_OFFSET equ 0x10000
 BOOT_INFO equ 0x500
 
 start_long_mode:
-    ; Update segment registers
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
@@ -17,13 +15,10 @@ start_long_mode:
     mov ss, ax
     mov rsp, 0x7FFFF
 
-    ; Pass boot info address in rdi
     mov rdi, BOOT_INFO
 
-    ; Jump to kernel
     mov rax, KERNEL_OFFSET
     call rax
     jmp $
 
-; Reuse GDT from boot.asm
 DATA_SEG equ 16
