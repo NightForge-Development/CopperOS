2b4e976e079ef2a4893f388f4aac6bcc7eb9926d
Scythe | Nyxon <159197373+ScytheMediaTheDev@users.noreply.github.com>
2025-04-28 18:34:41 -0400

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -4,32 +4,29 @@
 [org 0x7C00]
 
 ; Constants
-SECOND_STAGE_OFFSET equ 0x7000  ; Second stage load address
-BOOT32_OFFSET equ 0x8000        ; 32-bit stage load address
-BOOT64_OFFSET equ 0x9000        ; 64-bit stage load address
-KERNEL_OFFSET equ 0x10000       ; Kernel load address
-BOOT_INFO equ 0x500             ; Boot info structure address
-VGA_BUFFER equ 0xA0000          ; VGA memory
+SECOND_STAGE_OFFSET equ 0x7000
+BOOT32_OFFSET equ 0x8000
+BOOT64_OFFSET equ 0x9000
+KERNEL_OFFSET equ 0x10000
+BOOT_INFO equ 0x500
+VGA_BUFFER equ 0xA0000
 
 start:
-    ; Initialize registers and stack
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
 
-    ; Set VGA mode 13h (320x200, 256 colors)
     mov ah, 0x00
     mov al, 0x13
     int 0x10
 
-    ; Draw placeholder loading screen (red square)
     mov ax, 0xA000
     mov es, ax
     mov di, 320*100 + 100
     mov cx, 50
-    mov al, 0x04        ; Red color
+    mov al, 0x04
 .draw_loop:
     mov bx, cx
     mov cx, 50
@@ -38,31 +35,26 @@ start:
     add di, 320 - 50
     loop .draw_loop
 
-    ; Set up boot info structure
-    mov di, BO큼
-
+    mov di, BOOT_INFO
     mov eax, VGA_BUFFER
-    mov [di], eax       ; Store VGA buffer address
+    mov [di], eax
     xor eax, eax
-    mov [di + 4], eax   ; Reserved for future use
+    mov [di + 4], eax
 
-    ; Load second stage, boot32, boot64, and kernel
-    mov ah, 0x02        ; BIOS read sector
-    mov al, 32          ; Sectors to read
+    mov ah, 0x02
+    mov al, 32
     mov ch, 0
-    mov cl, 2           ; Start from sector 2
+    mov cl, 2
     mov dh, 0
-    mov dl, 0x80        ; Boot drive
+    mov dl, 0x80
     mov bx, SECOND_STAGE_OFFSET
     mov es, bx
     xor bx, bx
     int 0x13
     jc .disk_error
 
-    ; Call second stage to display full image
     call SECOND_STAGE_OFFSET
 
-    ; Switch to protected mode
     cli
     lgdt [gdt_descriptor]
     mov eax, cr0
@@ -80,17 +72,14 @@ start:
     int 0x10
     jmp .print
 
-; GDT
 gdt_start:
-    dq 0x0              ; Null descriptor
-    ; Code segment (32-bit)
+    dq 0x0
     dw 0xFFFF
     dw 0x0000
     db 0x00
     db 0x9A
     db 0xCF
     db 0x00
-    ; Data segment
     dw 0xFFFF
     dw 0x0000
     db 0x00
@@ -106,9 +95,7 @@ gdt_descriptor:
 CODE_SEG equ 8
 DATA_SEG equ 16
 
-; Data
 disk_error db 'Disk error!', 0
 
-; Boot sector signature
 times 510-($-$$) db 0
 dw 0xAA55
