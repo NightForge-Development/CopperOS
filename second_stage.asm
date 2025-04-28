2583eb405af34a3ea886e9120e7bf225b27dd2c5
Scythe | Nyxon <159197373+ScytheMediaTheDev@users.noreply.github.com>
2025-04-28 18:05:26 -0400

    Update second_stage.asm

--- second_stage.asm
+++ second_stage.asm
@@ -1,29 +1,16 @@
 ; second_stage.asm
+; Loads a 320x200 image for the loading screen
 [bits 16]
-mov ax, 0xA000
-mov es, ax
-xor di, di
-mov si, image_data
-mov cx, 320*200
-rep movsb
-; Jump to kernel
-cli
-lgdt [gdt_descriptor] ; Reuse bootloader's GDT
-mov eax, cr0
-or eax, 0x1
-mov cr0, eax
-jmp 8:protected_mode
+[org 0x7000]
 
-[bits 32]
-protected_mode:
-mov ax, 16
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-mov ss, ax
-mov esp, 0x9FFFF
-jmp KERNEL_OFFSET
+start:
+    mov ax, 0xA000      ; VGA buffer
+    mov es, ax
+    xor di, di
+    mov si, image_data
+    mov cx, 320*200
+    rep movsb
+    ret
 
 image_data:
     incbin "image.raw"
