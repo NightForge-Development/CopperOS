66e46c79db4daa594ac6a75fafc5395a89571f10
Scythe | Nyxon <159197373+ScytheMediaTheDev@users.noreply.github.com>
2025-04-28 18:35:06 -0400

    Update boot32.asm

--- boot32.asm
+++ boot32.asm
@@ -3,11 +3,9 @@
 [bits 32]
 [org 0x8000]
 
-; Constants
 BOOT64_OFFSET equ 0x9000
 
 start_protected:
-    ; Set up segment registers
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
@@ -16,24 +14,19 @@ start_protected:
     mov ss, ax
     mov esp, 0x7FFFF
 
-    ; Set up paging (identity map first 2MB)
-    mov edi, 0x1000     ; Page table base
+    mov edi, 0x1000
     xor eax, eax
-    mov ecx, 0x1000     ; Clear 4KB
+    mov ecx, 0x1000
     rep stosd
 
-    ; PML4 (0x1000)
     mov edi, 0x1000
     mov dword [edi], 0x2003
-    ; PDPT (0x2000)
     mov edi, 0x2000
     mov dword [edi], 0x3003
-    ; PD (0x3000)
     mov edi, 0x3000
     mov dword [edi], 0x4003
-    ; PT (0x4000): Map 2MB
     mov edi, 0x4000
-    mov eax, 0x3        ; Present, writable
+    mov eax, 0x3
     mov ecx, 512
 .map_page:
     mov [edi], eax
@@ -41,29 +34,23 @@ start_protected:
     add edi, 8
     loop .map_page
 
-    ; Enable PAE
     mov eax, cr4
-    or eax, 0x20        ; PAE bit
+    or eax, 0x20
     mov cr4, eax
 
-    ; Set page table
     mov eax, 0x1000
     mov cr3, eax
 
-    ; Enable long mode
-    mov ecx, 0xC0000080 ; EFER MSR
+    mov ecx, 0xC0000080
     rdmsr
-    or eax, 0x100       ; LME bit
+    or eax, 0x100
     wrmsr
 
-    ; Enable paging
     mov eax, cr0
-    or eax, 0x80000001  ; Paging + protected mode
+    or eax, 0x80000001
     mov cr0, eax
 
-    ; Jump to 64-bit stage
     jmp CODE_SEG:BOOT64_OFFSET
 
-; Reuse GDT from boot.asm
 CODE_SEG equ 8
 DATA_SEG equ 16
