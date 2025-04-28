8c3cd0dc764b7187299d261b8f703745781d5573
Scythe | Nyxon <159197373+ScytheMediaTheDev@users.noreply.github.com>
2025-04-28 18:11:51 -0400

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,6 +1,11 @@
 // kernel.c
-void kmain(void) {
-    char *vga = (char *)0xA0000;
+typedef struct {
+    unsigned long vga_buffer; // VGA buffer address
+    unsigned long reserved;   // For future use
+} BootInfo;
+
+void kmain(BootInfo *boot_info) {
+    char *vga = (char *)boot_info->vga_buffer;
     for (int i = 0; i < 320 * 200; i++) {
         vga[i] = 0x01; // Blue color
     }
